ea662ecbbe4bda339f4c98f7b5f756573617e354
Unknown <ligno19@gmail.com>
2018-03-23 08:42:44 -0400

    Managers up

--- Assets/Scripts/Managers/FlowManager.cs
+++ Assets/Scripts/Managers/FlowManager.cs
@@ -4,7 +4,28 @@ using UnityEngine;
 
 public class FlowManager {
 
-	
+    #region Singleton
+    private static FlowManager instance;
 
+    private FlowManager() { }
+
+    public static FlowManager Instance {
+        get {
+            if (instance == null) {
+                instance = new FlowManager();
+            }
+            return instance;
+        }
+    }
+
+    #endregion
+
+    public void Start() {
+
+    }
+
+    public void Update() {
+
+    }
 
 }
---
+++ Assets/Scripts/Managers/MainStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+        FlowManager.Instance.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FlowManager.Instance.Update();
+	}
+}
